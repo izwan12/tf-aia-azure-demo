d134fd99e16f6f3757563a1e11bba9dd4e903b00
Izwan <izwan0124@proton.me>
2025-10-13 03:13:44 +0800

    updates

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "webapp_url" {
   description = "URLs for all deployed environments"
-  value       = { for k, m in module.webapps : k => m.app_url }
+  value       = { for k, m in module.webapp : k => m.app_url }
 }
