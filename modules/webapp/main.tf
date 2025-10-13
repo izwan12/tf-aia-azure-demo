e12c69946bb405e1df543c21effcc78e64c286f6
Izwan <izwan0124@proton.me>
2025-10-13 22:25:47 +0800

    updates

--- modules/webapp/main.tf
+++ modules/webapp/main.tf
@@ -23,11 +23,11 @@ resource "azurerm_linux_web_app" "this" {
       docker_registry_url = var.docker_registry
     }
     always_on = false
+
+    app_command_line = "/bin/sh -c 'mkdir -p /home/site/wwwroot && printf \"This is ${var.env} environment.\" > /home/site/wwwroot/index.html && rm -rf /usr/local/apache2/htdocs/* && ln -s /home/site/wwwroot /usr/local/apache2/htdocs && exec httpd-foreground'"
   }
 
   app_settings = {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "true"
-    "ENVIRONMENT"                         = var.env
-    "STARTUP_COMMAND"                     = "/bin/sh -c 'echo This is $ENVIRONMENT environment. > /usr/local/apache2/htdocs/index.html && exec httpd-foreground'"
   }
 }
